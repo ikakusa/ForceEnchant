5614c7cb9fdc4a17c06003b917d30b74359550c9
誠 遺憾 <ikasan2860@gmail.com>
2024-09-22 22:36:31 +0900

    ouou

--- ForceEnchant/Force/Hooks/CommandHook/CommandHook.cpp
+++ ForceEnchant/Force/Hooks/CommandHook/CommandHook.cpp
@@ -1,6 +1,6 @@
 #include "CommandHook.h"
 
 bool CommandHook::ValidRange::handle(int input, int low, int high, __int64 out) {
-	static auto oFunc = funcPtr->GetFastcall<bool, unsigned char, bool>();
+	static auto oFunc = funcPtr->GetFastcall<bool, int, int, int, __int64>();
 	return true;
 }
