d9f86b1573653d4828298be3c22ae9d263249bdb
誠 遺憾 <ikasan2860@gmail.com>
2026-05-06 12:35:29 +0900

    removed Tanaka class

--- ForceEnchant/Force/SDK/EnchantResult.h
+++ ForceEnchant/Force/SDK/EnchantResult.h
@@ -13,14 +13,3 @@ public:
 	BUILD_ACCESS(int, enchantId, 0x8);
 	BUILD_ACCESS(int, level, 0x10);
 };
-
-class Tanaka {
-	int age; // 0x0
-	struct Kanojo { // 0x4
-		int age; // 0x4
-		int three_size; //0x8
-	};
-	struct Oya {
-
-	};
-};
